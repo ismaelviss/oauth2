5a392e61834b902b913fe0558ae5f1d52928d88d
Elvis Salvatierra <ismaelviss@gmail.com>
2022-02-11 00:59:33 -0500

    validacion de jwt

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParse = require("body-parser");
 const jwt = require("jsonwebtoken");
 const { config } = require("./config");
+const { json } = require('body-parser');
 
 const app = express();
 
@@ -13,6 +14,17 @@ app.post("/api/auth/token", function(req, res){
     const { email, username, name } = req.body;
     const token = jwt.sign({sub: username, email, name}, config.authJwtSecret);
     res.json({access_token: token });
+});
+
+app.get("/api/auth/verify", function(req, res, next) {
+    const { access_token } = req.query
+
+    try {
+        const decoded = jwt.verify(access_token, config.authJwtSecret)
+        res.json({message: "the access token is valid", username: decoded.sub})
+    } catch(err) {
+        next(err);
+    }
 });
 
 const server = app.listen(5001, function() {

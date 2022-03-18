8136dedff8d83c13aac2e49c6c4ea173f3902768
Elvis Salvatierra <ismaelviss@gmail.com>
2022-03-17 19:29:10 -0500

    client credentials

--- api/config/index.js
+++ api/config/index.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 const config = {
     authJwtSecret: process.env.AUTH_JWT_SECRET,
-    authJwksSecret: process.env.AUTH_JWKS_PRIVATE_SECRET
+    authJwksSecret: process.env.AUTH_JWKS_PRIVATE_SECRET,
+    spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
+    spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET
 }; 
 
 module.exports = { config: config };
--- api/index.js
+++ api/index.js
@@ -3,12 +3,40 @@ const bodyParse = require("body-parser");
 const jwt = require("jsonwebtoken");
 const { config } = require("./config");
 const { json } = require('body-parser');
+const cors = require("cors");
+const request = require('request');
+const encodeBasic = require("./utils/encodeBasic");
 
 const app = express();
 
+const corsOptions = { origin: "http://example.com" };
+
+app.use(cors(corsOptions));
 
 //body parser
 app.use(bodyParse.json());
+
+function getUserPlaylists(accessToken, userId) {
+    if (!accessToken || !userId) {
+      return Promise.resolve(null);
+    }
+  
+    const options = {
+      url: `https://api.spotify.com/v1/users/${userId}/playlists`,
+      headers: { Authorization: `Bearer ${accessToken}` },
+      json: true
+    };
+  
+    return new Promise((resolve, reject) => {
+      request.get(options, function(error, response, body) {
+        if (error || response.statusCode !== 200) {
+          reject(error);
+        }
+        resolve(body);
+        
+      });
+    });
+  }
 
 app.post("/api/auth/token", function(req, res){
     const { email, username, name } = req.body;
@@ -31,6 +59,31 @@ app.get("/api/auth/verify", function(req, res, next) {
     } catch(err) {
         next(err);
     }
+});
+
+app.get("/api/playlists", async function(req, res, next) {
+    const { userId } = req.query;
+
+    const authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+            Authorization: `Basic ${encodeBasic(config.spotifyClientId, config.spotifyClientSecret)}`
+        },
+        form: {
+            grant_type: "client_credentials"
+        },
+        json: true
+    }
+
+    request.post(authOptions, async function(error, response, body){
+        if(error || response.statusCode !== 200) {
+            next(error);
+        }
+
+        const accessToken = body.access_token;
+        const userPlayLists = await getUserPlaylists(accessToken, userId);
+        res.json({ playlists: userPlayLists });
+    });
 });
 
 const server = app.listen(5001, function() {
